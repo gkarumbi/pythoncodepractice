f812f5a5c31eb23bd24f7d73dbf857ae7051f274
gkarumbi <55401368+gkarumbi@users.noreply.github.com>
2019-10-27 11:02:51 +0300

    Add files via upload

--- Parent.py
+++ Parent.py
@@ -10,3 +10,14 @@ class Parent:
 p = Parent("Hazel", 6)
 p.getAge(2019,1988)
 print(str(p.getAge(2019,1988)))
+
+class Child(Parent):
+
+    def __init__(self, eye_color = " ", Height= 0, skin_color=" "):
+        # super.__init__(self,eye_color= " ", Height = 0)
+        self.skin_color = skin_color
+
+
+c = Child("Brown", 5)
+c.getAge(2019,2000)
+print(str(c.getAge(2019,2000)))
